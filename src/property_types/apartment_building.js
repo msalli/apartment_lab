90a30c0ad8648234e7ff2616d22f898d21b4891d
msalli <alli.cernoch@gmail.com>
2014-07-21 09:19:44 -0700

    Minor changes, no functions

--- src/property_types/apartment_building.js
+++ src/property_types/apartment_building.js
@@ -1,7 +1,14 @@
-var property = require("./property.js");
+var Property = require("./property.js");
+var Inherit = require("./../inherit.js");
 
 function ApartmentBuilding(name, address) {
   // A building can have many many units 
+  this.name = name;
+  this.address = address;
+  this.units = []
 }
+
+Inherit(ApartmentBuilding, Property);
+
 
 module.exports = ApartmentBuilding;
