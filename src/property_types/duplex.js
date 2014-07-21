0ec5d68005e3345320e2420112b3add43533dca4
msalli <alli.cernoch@gmail.com>
2014-07-21 09:20:21 -0700

    Minor changes, no functions

--- src/property_types/duplex.js
+++ src/property_types/duplex.js
@@ -1,6 +1,15 @@
-var property = require("./property");
+var Property = require("./property");
+var Inherit = require("./../inherit.js");
 
 function Duplex(address) {
   // A duplex only has 2 units;
+  this.address = address;
+  this.units = [];
 }
+
+Inherit(Duplex, Property);
+
+
+
 module.exports = Duplex;
+
