a7c3053b92fa674975673a2e228f5400ca5d714a
msalli <alli.cernoch@gmail.com>
2014-07-21 09:19:22 -0700

    Minor changes, no functions

--- src/property_types/town_house.js
+++ src/property_types/town_house.js
@@ -1,11 +1,21 @@
-var property = require("./property");
+var Property = require("./property");
+var Inherit = require("./../inherit.js");
 
 function TownHouse(address){
   // only has one unit per address
+  this.address = address;
+  this.unit = 1;
+  this.tenantSet = false;
 };
 
+//when i set a tenant, also change tenantSet to true
+
+Inherit(TownHouse, Property);
+
+
 TownHouse.prototype.available = function(){
-  // a tenant it should not be available
+  // if it has a tenant it should not be available
+
 };
 
 module.exports = TownHouse;
