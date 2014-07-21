236a5c2095c65cc8fc3a15c0023dc64141efc0ee
msalli <alli.cernoch@gmail.com>
2014-07-21 09:16:22 -0700

    Complete

--- src/people/tenant.js
+++ src/people/tenant.js
@@ -1,12 +1,19 @@
-var person = require("./person.js");
+var Person = require("./person.js");
+var Inherit = require("./../inherit.js");
 
 function Tenant(name, contact) {
-  // set this.name, and contact
+	this.name = name;
+	this.contact = contact;
+	//set in prototype
   this.references = [];
 };
 
+Inherit(Tenant, Person);
+
 Tenant.prototype.addReference = function(reference){
-  // add reference to references
+	return this.references.push(reference);
+	console.log(this.name + " has " + reference + "as a new reference!");
 };
+
 
 module.exports = Tenant;
