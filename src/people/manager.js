9acf6ab68e92d0cf4ba403687b432d7e74821ac7
msalli <alli.cernoch@gmail.com>
2014-07-21 09:14:53 -0700

    Complete

--- src/people/manager.js
+++ src/people/manager.js
@@ -1,17 +1,29 @@
-var person = require("./person");
+var Person = require("./person");
+var Inherit = require("./../inherit.js");
 
 function Manager(name, contact) {
-  // set name and contact
-  
+  this.name = name;
+  this.contact = contact;  
   this.properties = [];
 }
 
+Inherit(Manager, Person);
+
 Manager.prototype.addProperty = function(property) {
-  // add property from properties
+  this.properties.push(property);
+  // console.log(this.name + " now owns " + property + "!");
 };
 
 Manager.prototype.removeProperty = function(property) {
-  // remove properties
+  for (var i = 0; i < this.properties.length; i++) {
+  	if (this.properties[i] === property) {
+  		this.properties.splice(i, 1);
+  		// console.log(property + "has been removed.");
+  	}
+  }
 };
+
+var manager1 = new Manager("Queen Cersei", "888-888-8888")
+
 
 module.exports = Manager;
