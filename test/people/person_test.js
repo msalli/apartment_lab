7e82965e0f7884cc2b38f7487ea555bafd93d491
msalli <alli.cernoch@gmail.com>
2014-07-21 09:25:49 -0700

    Complete

--- test/people/person_test.js
+++ test/people/person_test.js
@@ -1,8 +1,15 @@
 var expect = require("chai").expect,
   Person = require("../../src/people/person.js");
 
-console.log("Person test")
 
 describe("Person", function(){
+  var me = new Person("Alli", "000-0000");
+  // var reference1 = new Person("Bob", "999-9999");
+  // var reference2 = new Person("Fred", "999-9999");
   
+  it("should construct a new person", function(){
+  	expect(me.name).to.equal("Alli");
+  	expect(me.contact).to.equal("000-0000");
+  });  
 });
+
